8d9b25bef6ef249dbc154341d92f10fbe33760a8
Robin Doirin <robindoirin@gmail.com>
2019-10-06 16:16:56 +0000

    Automatically backed up by Learn

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -1,6 +1,6 @@
 class BirdsController < ApplicationController
   def index
     @birds = Bird.all
-    render plain: "Hello #{@birds[3].name}"
+    render json: { message: "Hashes of data will converted to JSON"}
   end
 end
